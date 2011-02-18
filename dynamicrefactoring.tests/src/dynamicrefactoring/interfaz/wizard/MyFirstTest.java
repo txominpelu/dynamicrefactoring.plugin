66468efae4f36ade9e9552e0dabe716f832cb834
imediava <imediava2@hotmail.com>
2011-02-18 11:32:06 +0100

    Mas pruebas del build 3.

--- dynamicrefactoring.tests/src/dynamicrefactoring/interfaz/wizard/MyFirstTest.java
+++ dynamicrefactoring.tests/src/dynamicrefactoring/interfaz/wizard/MyFirstTest.java
@@ -1,9 +1,14 @@
 package dynamicrefactoring.interfaz.wizard;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.results.VoidResult;
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.ui.PlatformUI;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +23,32 @@ public class MyFirstTest {
 		bot = new SWTWorkbenchBot();
 		bot.viewByTitle("Welcome").close();
 	}
+	
+	@Before
+	public final void waitMenu() {
+        // The following seems to be needed to run SWTBot tests in Xvfb (the 'fake' X-server).
+        // see http://dev.eclipse.org/mhonarc/newsLists/news.eclipse.swtbot/msg01134.html
+        UIThreadRunnable.syncExec(new VoidResult() {
+            public void run() {
+                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()
+                        .forceActive();
+            }
+        });
+
+		bot.waitUntil(new DefaultCondition() {
+
+			public boolean test() throws Exception {
+				return bot.menu("File").isEnabled();
+			}
+
+			public String getFailureMessage() {
+				return "Menu bar not available";
+			}
+		});
+	}
+
+
+	
  
  
 	@Test
