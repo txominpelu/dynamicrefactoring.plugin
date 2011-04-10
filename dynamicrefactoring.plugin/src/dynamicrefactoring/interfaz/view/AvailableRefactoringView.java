fc2636a549aaab4f427bd6c183fea80d95c4d82e
Míryam Gómez San Martín <miryam.gsm@gmail.com>
2011-04-10 20:10:13 +0200

    .

--- dynamicrefactoring.plugin/src/dynamicrefactoring/interfaz/view/AvailableRefactoringView.java
+++ dynamicrefactoring.plugin/src/dynamicrefactoring/interfaz/view/AvailableRefactoringView.java
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 package dynamicrefactoring.interfaz.view;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
